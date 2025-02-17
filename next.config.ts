dfe9afdf06d51fa9641c3e49370cc8807597b393
yuy26 <yuy26@trip.com>
2025-02-17 15:55:01 +0800

    chapter 7

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.externals = [...config.externals, "bcrypt"];
-    return config;
-  },
-
 };
 
 export default nextConfig;
+
